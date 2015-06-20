7a83ab42135eb596765cf78d956df2f594594eff
Laurent Younes <laurent.younes@jhu.edu>
2015-06-20 11:26:57 -0400

    ...

--- py-lddmm/biocardLoopWithThreads.py
+++ py-lddmm/biocardLoopWithThreads.py
@@ -1,6 +1,4 @@
 import csv
-import numpy as np
-import logging
 import loggingUtils
 import surfaces
 from surfaces import *
@@ -20,20 +18,19 @@ def threadfun(q):
             print 'Exception'
         q.task_done()
 
-def runLongitudinalSurface(minL=3, atrophy=False):
+def runLongitudinalSurface(template, targetList, minL=3,atrophy=False, resultDir='.'):
     if atrophy:
         import surfaceTimeSeriesAtrophy as match
     else:
         import surfaceTimeSeries as match
 
+    os.environ['OMP_NUM_THREADS'] = 4
+    max_proc = 5
     
     with open('/cis/home/younes/MATLAB/shapeFun/CA_STUDIES/BIOCARD/filelist.txt','r') as csvf:
         rdr = list(csv.DictReader(csvf,delimiter=',',fieldnames=('lab','isleft','id','filename')))
         files = []
-        lab = np.zeros(len(rdr), dtype=long) ;
-        lr = np.zeros(len(rdr), dtype=long) ;
         previousLab = 0
-        j = 0 
         currentFile = []
         for row in rdr:
             if int(row['lab']) == previousLab:
@@ -54,17 +51,17 @@ def runLongitudinalSurface(minL=3, atrophy=False):
     #outputDir = '/cis/home/younes/MorphingData/twoBallsStitched'
     #outputDir = '/Users/younes/Development/Results/tight_stitched_rigid2_10'
 
-    rdir = '/cis/project/biocard/data/2mm_complete_set_surface_mapping_10212012/hippocampus/6_mappings_baseline_template_all/0_template_to_all/' ;
     fv0 = surfaces.Surface(filename='/cis/project/biocard/data/2mm_complete_set_surface_mapping_10212012/hippocampus/4_create_population_based_template/newTemplate.byu')
     K1 = Kernel(name='laplacian', sigma = 6.5, order=4)
     sm = surfaceMatching.SurfaceMatchingParam(timeStep=0.1, KparDiff=K1, sigmaDist=2.5, sigmaError=1., errorType='varifold')
 
     q = Queue.Queue()
     #files = [files[1],files[5],files[8]]
-    files = [files[9]]
-    for k,s in enumerate(files):
-        fv = []
-        print s
+    #selected = range(len(files)) 
+    selected = (1,9) 
+    for k in selected:
+        s = files[k]
+        print s[0]
         for fn in s:
                 try:
                     fv += [surfaces.Surface(filename=fn+'.byu')]
@@ -74,7 +71,7 @@ def runLongitudinalSurface(minL=3, atrophy=False):
 
         outputDir = '/cis/home/younes/Results/biocardTS/piecewise_NA_'+str(k)
         if __name__ == "__main__":
-            loggingUtils.setup_default_logging(info_outputDir, fileName='info', stdOutput=(k==0))
+            loggingUtils.setup_default_logging(info_outputDir, fileName='info', stdOutput=(k==selected[0]))
         else:
             loggingUtils.setup_default_logging(fileName='info')
 
@@ -91,7 +88,7 @@ def runLongitudinalSurface(minL=3, atrophy=False):
         #, affine='none', rotWeight=0.1))
         q.put(f)
 
-    for k in range(1):
+    for k in range(max_proc):
         w = threading.Thread(target=threadfun, args=(q,))
         w.setDaemon(True)
         w.start()
@@ -101,4 +98,15 @@ def runLongitudinalSurface(minL=3, atrophy=False):
 
 
 if __name__=="__main__":
-    runLongitudinalSurface(atrophy=True)
+    parser = argparse.ArgumentParser(description='runs longitudinal surface matching based on an input file')
+    parser.add_argument('template', metavar='template', type = str, help='template')
+    parser.add_argument('targetList', metavar='targetlist', type = str, help='file containing the list of targets')
+    parser.add_argument('--results', metavar = 'resultDir', type = str, dest = 'resultDir', default = '.', help='Output directory')
+    args = parser.parse_args()
+    
+    #template: /cis/project/biocard/data/2mm_complete_set_surface_mapping_10212012/hippocampus/4_create_population_based_template/newTemplate.byu'
+    #targetList: '/cis/home/younes/MATLAB/shapeFun/CA_STUDIES/BIOCARD/filelist.txt'
+    #Results: '/cis/home/younes/Results/biocardTS/withAtrophy'
+    
+    
+    runLongitudinalSurface(args.template, args.targetList, atrophy=True, resultDir=args.resultDir)
