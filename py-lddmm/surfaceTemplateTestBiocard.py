f8b37080a01991a6d18672e5f9e1f4136776cef8
Laurent Younes <laurent.younes@jhu.edu>
2016-09-02 10:10:23 -0400

    ...

--- py-lddmm/surfaceTemplateTestBiocard.py
+++ py-lddmm/surfaceTemplateTestBiocard.py
@@ -15,9 +15,9 @@ def main():
     K0 = Kernel(name='gauss', sigma = 1.0)
     Kdist = Kernel(name='gauss', sigma = 2.5)
 
-    loggingUtils.setup_default_logging('/Users/younes/Results/surfaceTemplateBiocard', fileName='info.txt', stdOutput = True)
+    loggingUtils.setup_default_logging('/cis/home/younes/Development/Results/surfaceTemplateBiocard', fileName='info.txt', stdOutput = True)
     sm = SurfaceTemplateParam(timeStep=0.1, KparDiff=K1, KparDiff0 = K0, KparDist=Kdist, sigmaError=1., errorType='varifold')# internalCost='h1')
-    f = SurfaceTemplate(HyperTmpl=fv1[0], Targets=fv1, outputDir='/Users/younes/Results/surfaceTemplateBiocard',param=sm, testGradient=True, 
+    f = SurfaceTemplate(HyperTmpl=fv1[0], Targets=fv1, outputDir='/cis/home/younes/Development/Results/surfaceTemplateBiocard',param=sm, testGradient=True, 
                         lambdaPrior = 0.01,
                         maxIter=1000, affine='none', rotWeight=1., transWeight = 1., scaleWeight=10., affineWeight=100.)
     f.computeTemplate()
