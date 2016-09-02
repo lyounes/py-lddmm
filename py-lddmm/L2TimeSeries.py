c4fe841b5b8971544d13f30388858ec51c81694d
Laurent Younes <laurent.younes@jhu.edu>
2016-09-02 10:08:30 -0400

    ...

--- py-lddmm/L2TimeSeries.py
+++ py-lddmm/L2TimeSeries.py
@@ -5,29 +5,28 @@ from kernelFunctions import *
 import surfaceMatching
 #import secondOrderMatching as match
 
-def compute(Atrophy=False):
+def compute(tmpl, targetDir, outputDir, display=True, Atrophy=False):
     if Atrophy:
         import surfaceTimeSeriesAtrophy as match
     else:
         import surfaceTimeSeries as match
 
     #outputDir = '/Users/younes/Development/Results/biocardTS/spline'
-    outputDir = '/Users/younes/Development/Results/L2TimeSeriesAtrophy'
+    #outputDir = '/Users/younes/Development/Results/L2TimeSeriesAtrophy'
     #outputDir = '/cis/home/younes/MorphingData/twoBallsStitched'
     #outputDir = '/Users/younes/Development/Results/tight_stitched_rigid2_10'
     if __name__ == "__main__":
-        loggingUtils.setup_default_logging(outputDir, fileName='info', stdOutput = True)
+        loggingUtils.setup_default_logging(outputDir, fileName='info', stdOutput=display)
     else:
         loggingUtils.setup_default_logging()
 
-    rdir = '/Users/younes/Development/Data/TimeseriesResults/' ;
-    sub = '016_S_4584_L'
-    nscan = 4
-    #sub = '2729611'
     fv = []
-    fv0 = surfaces.Surface(filename='/Users/younes/Development/Data/TimeseriesResults/estimatedTemplate.byu')
-    for k in range(nscan):
-        fv = fv + [rdir + sub + '/imageOutput_time_{0:d}_channel_0.vtk'.format(k) ]
+    fv0 = surfaces.Surface(filename=tmpl)
+    j = 0 
+    print targetDir+'/imageOutput_time_{0:d}_channel_0.vtk'.format(j)
+    while os.path.exists(targetDir+'/imageOutput_time_{0:d}_channel_0.vtk'.format(j)):
+        fv = fv + [targetDir + '/imageOutput_time_{0:d}_channel_0.vtk'.format(j) ]
+        j += 1
 
     ## Object kernel
     K1 = Kernel(name='laplacian', sigma = 2.5, order=4)
@@ -46,5 +45,11 @@ def compute(Atrophy=False):
     return f
 
 if __name__=="__main__":
-    compute(True)
+    parser = argparse.ArgumentParser(description='runs first order longitudinal surface registration')
+    parser.add_argument('sub', metavar='sub', type = str, help='subject directory')
+    args = parser.parse_args()
+    #print args.sub
+    compute('/cis/home/younes/MorphingData/TimeseriesResults/estimatedTemplate.byu', 
+            '/cis/home/younes/MorphingData/TimeseriesResults/' + args.sub, 
+            '/cis/home/younes/Development/Results/L2TimeSeriesAtrophy/'+ args.sub, display=False, Atrophy=True)
 
