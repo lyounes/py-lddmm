c5c3ce55d7d14e2e8401066a879a8879d2dd84c8
Laurent Younes <laurent.younes@jhu.edu>
2016-10-06 11:58:04 -0400

    ...

--- py-lddmm/createLongitudinalScripts.py
+++ py-lddmm/createLongitudinalScripts.py
@@ -29,7 +29,7 @@ def createLongitudinalSurfaceScripts(minL=3):
                 fname.write('cd '+ source +'\n')
                 #fname.write('source ~/.bashrc\n which python\n')
                 #fname.write('which python\necho $PATH\necho $LD_LIBRARY_PATH\necho $SHELL\n')
-                fname.write('python L2TimeSeries.py ' +  os.path.basename(d) + '--display --atrophy --rescale\n')
+                fname.write('python L2TimeSeries.py ' +  os.path.basename(d) + '  --display --atrophy --rescale\n')
             cstr = "qsub  " + shname
             print cstr
             #subprocess.call(cstr, shell=True)
